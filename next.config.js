7b76cd37e37dfa72b149845969e0508f4751a7fd
Przem <przemkowski@icloud.com>
2026-03-14 12:06:59 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const apiTarget =
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: process.cwd(),
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
